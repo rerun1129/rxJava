347647167c7bd9cdfb5df8e518ae8e6f46f3f55f
rerun1129 <a01021719359@gmail.com>
2023-09-03 17:28:05 +0900

    파일 추가 및 웹플럭스 의존성 추가

--- src/main/java/com/example/rxjava/example/DeclarativeProgramming.java
+++ src/main/java/com/example/rxjava/example/DeclarativeProgramming.java
@@ -1,2 +1,38 @@
-package com.example.rxjava.example;public class DeclarativeProgramming {
+package com.example.rxjava.example;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxProcessor;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Sinks;
+
+public class DeclarativeProgramming {
+    public static void main ( String[] args ) throws InterruptedException {
+//        List <Integer> integerList = Arrays.asList ( 1, 3, 21, 10, 8, 11 );
+//        int result = integerList.stream ().filter ( n -> n > 6 && (n % 2 != 0) ).mapToInt ( n -> n ).sum ();
+//        System.out.println ( result );
+
+//        Flux.just ( 100, 200, 300, 400, 500 )
+//            .doOnNext ( item -> System.out.println ( Thread.currentThread ().getName () + ":" + item ) )
+//            .subscribeOn ( Schedulers.parallel () )
+//            .publishOn ( Schedulers.boundedElastic () )
+//            .filter ( n -> n > 300 )
+//            .subscribe (item -> System.out.println ( Thread.currentThread ().getName () + ":" + item ) );
+//        Thread.sleep ( 500 );
+
+//        Flux.just ( 1,25,9,15,7,30)
+//            .filter ( x -> x > 10 )
+//            .subscribe (System.out::println);
+//        Flux <String> just = Flux.just ( "Hello", "RxJava" );
+//        just.subscribe (System.out::println);
+//        Flux <Integer> integerFlux = Flux.just ( 1, 3, 5, 7 );
+//        integerFlux.subscribe (x -> System.out.println ( "구독자 1 :" + x ));
+//        integerFlux.subscribe (x -> System.out.println ( "구독자 2 :" + x ));
+        Sinks.Many <Integer> many = Sinks.many ( ).multicast ( ).onBackpressureBuffer ( );
+        many.asFlux ().subscribe (x -> System.out.println ( "구독자 1 :" + x ));
+        many.emitNext ( 1, Sinks.EmitFailureHandler.FAIL_FAST );
+        many.emitNext ( 3, Sinks.EmitFailureHandler.FAIL_FAST );
+        many.asFlux ().subscribe (x -> System.out.println ( "구독자 2 :" + x ));
+        many.emitNext ( 5, Sinks.EmitFailureHandler.FAIL_FAST );
+        many.emitNext ( 7, Sinks.EmitFailureHandler.FAIL_FAST );
+    }
 }
--- src/main/java/com/example/rxjava/example/ImperativeProgramming.java
+++ src/main/java/com/example/rxjava/example/ImperativeProgramming.java
@@ -1,5 +1,17 @@
 package com.example.rxjava.example;
 
-public class DeclarativeProgramming {
+import java.util.Arrays;
+import java.util.List;
 
+public class ImperativeProgramming {
+    public static void main ( String[] args ) {
+        List <Integer> integerList = Arrays.asList ( 1, 3, 21, 10, 8, 11 );
+        int sum = 0;
+        for ( int integer : integerList ) {
+            if(integer > 6 && (integer % 2 != 0)){
+                sum += integer;
+            }
+        }
+        System.out.println ( sum );
+    }
 }
