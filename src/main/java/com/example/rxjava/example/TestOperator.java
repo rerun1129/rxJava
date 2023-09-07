ac8013e9082f4c0f92cf266c7cb3d4c9a27a1789
rerun1129 <a01021719359@gmail.com>
2023-09-07 23:54:21 +0900

    파일 추가 및 웹플럭스 의존성 추가

--- src/main/java/com/example/rxjava/example/TestOperator.java
+++ src/main/java/com/example/rxjava/example/TestOperator.java
@@ -1,29 +1,38 @@
 package com.example.rxjava.example;
 
+import com.example.rxjava.example.common.Car;
+import com.example.rxjava.example.common.CarMaker;
+import com.example.rxjava.example.common.SampleData;
+import com.example.rxjava.example.utils.LogType;
+import com.example.rxjava.example.utils.Logger;
 import com.example.rxjava.example.utils.TimeUtil;
+import io.reactivex.Observable;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.time.Duration.ofMillis;
 
 @Slf4j
 public class TestOperator {
     public static void main ( String[] args ) {
-        Flux.just ( 10, 12, 15, 16 )
-            .zipWith (Flux.just ( 1,2,0,4 ) ,(a,b) -> {
-                        try {
-                            return (a / b);
-                        }catch ( ArithmeticException ex ){
-                            log.error ( "{}", ex.getMessage () );
-                            throw ex;
-                        }
-            })
-            .retry (3)
-            .onErrorReturn ( -1 )
-            .subscribe (
-                data -> log.info ( "{}", data ),
-                error -> log.error ( "{}", error.getMessage () )
-            );
-        TimeUtil.sleep ( 5000 );
+        Flux <CarMaker> flux1 =
+                Flux.fromArray ( SampleData.carMakers )
+                    .subscribeOn ( Schedulers.immediate () )
+                    .delaySequence ( ofMillis ( 500 ) )
+                    .doOnNext ( data -> log.info ( "Flux1 : {}", data ) );
+        Flux <CarMaker> flux2 =
+                Flux.fromArray ( SampleData.carMakersDuplicated )
+                    .delaySequence ( ofMillis ( 1000 ) )
+                    .doOnNext ( data -> log.info ( "Flux2 : {}", data ) );
+        Mono.sequenceEqual ( flux1, flux2 )
+            .subscribe ( data -> log.info ( "{}", data ) );
+        TimeUtil.sleep ( 2000 );
     }
+
 }
