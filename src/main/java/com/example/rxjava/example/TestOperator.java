e66adcc1baa26adfc2e2c84da8d5fb23c6d387ec
KoYunSeok <a01021719359@gmail.com>
2023-09-07 17:42:10 +0900

    추가

--- src/main/java/com/example/rxjava/example/TestOperator.java
+++ src/main/java/com/example/rxjava/example/TestOperator.java
@@ -1,29 +1,29 @@
 package com.example.rxjava.example;
 
+import com.example.rxjava.example.common.SampleData;
 import com.example.rxjava.example.utils.TimeUtil;
+import io.reactivex.Observable;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
+import reactor.util.function.Tuples;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.time.Duration.ofMillis;
 
 @Slf4j
 public class TestOperator {
     public static void main ( String[] args ) {
-        Flux.just ( 10, 12, 15, 16 )
-            .zipWith (Flux.just ( 1,2,0,4 ) ,(a,b) -> {
-                        try {
-                            return (a / b);
-                        }catch ( ArithmeticException ex ){
-                            log.error ( "{}", ex.getMessage () );
-                            throw ex;
-                        }
-            })
-            .retry (3)
-            .onErrorReturn ( -1 )
-            .subscribe (
-                data -> log.info ( "{}", data ),
-                error -> log.error ( "{}", error.getMessage () )
-            );
-        TimeUtil.sleep ( 5000 );
+        List <Flux <Integer>> fluxList = Arrays.asList (
+                Flux.fromIterable ( SampleData.salesOfBranchA ),
+                Flux.fromIterable ( SampleData.salesOfBranchB ),
+                Flux.fromIterable ( SampleData.salesOfBranchC )
+        );
+
+        Flux.zip ( fluxList, flux -> Arrays.stream( flux ).reduce (0, (acc, a2) -> (int)acc + (int)a2 ) )
+            .index ((index, value) -> index + 1 + "월 총매출 : " + value )
+            .subscribe (data -> log.info ( "{}", data ));
     }
 }
